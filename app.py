5c01cf25025fe1080ebf287a2a30a00efa78fb39
manatoishihara <s2422088@stu.musashino-u.ac.jp>
2025-10-09 19:17:21 +0900

    SECRET_KEYの二重指定を防ぐ

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # app.py
-from app import create_app   # ← app/__init__.py の create_app をインポート
+from app import create_app
 
+# Flaskアプリ生成
 app = create_app()
-app.secret_key = "your_secret_key_here"  # 一度決めたら変えない　ここでsessionを切れないようにしている
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("アクセスURL: http://localhost:5055")
     app.run(debug=True, port=5055)
